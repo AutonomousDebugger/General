973203c25e4f5dec3ed7937ee5819c7ffdfb3789
AutonomousDebugger <personalfor854@gmail.com>
2026-05-23 01:03:16 -0700

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function changeText() {
+  document.getElementById("message").textContent =
+    "Yo, JavaScript is working on your website!";
+}
 
+function changeTheme() {
+  document.body.classList.toggle("light-mode");
+}
+
+function showTime() {
+  const now = new Date();
+  document.getElementById("time").textContent =
+    "Current time: " + now.toLocaleTimeString();
+}
